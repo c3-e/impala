93e78fe8ddad01f8b53edda6eaf76c1fb77ae1d6
Sean Ahn <sean.ahn@c3iot.com>
2021-08-27 13:31:04 -0700

    openldap and ldap authz added

--- c3-impala-shell.sh
+++ c3-impala-shell.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 docker run --network=quickstart-network -it \
-     ${IMPALA_QUICKSTART_IMAGE_PREFIX}impala_quickstart_client impala-shell
+     ${IMPALA_QUICKSTART_IMAGE_PREFIX}impala_quickstart_client impala-shell -l --auth_creds_ok_in_clear -u impala --ldap_password_cmd="echo -n c3impala"
+
+## bash first then run impala-shell
+# docker run --network=quickstart-network -it      ${IMPALA_QUICKSTART_IMAGE_PREFIX}impala_quickstart_client bash
+# impala-shell -i impalad-1:21000 -l --auth_creds_ok_in_clear -u impala --ldap_password_cmd="echo -n c3impala"
+
+## no authentication
+# docker run --network=quickstart-network -it \
+#     ${IMPALA_QUICKSTART_IMAGE_PREFIX}impala_quickstart_client impala-shell
