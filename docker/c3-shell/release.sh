718d3387def9060bed9ad95f4ae6608eede8ce07
Sean Ahn <sean.ahn@c3iot.com>
2021-09-02 14:23:40 -0700

    c3 add b3

---
+++ docker/c3-shell/release.sh
@@ -0,0 +1,6 @@
+export VER=1.5
+
+docker build -t c3ai/c3-impala-shell:$VER .
+docker tag c3ai/c3-impala-shell:$VER c3ai/c3-impala-shell:latest
+docker push c3ai/c3-impala-shell:$VER
+docker push c3ai/c3-impala-shell:latest
