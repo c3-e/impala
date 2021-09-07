9a6e9e238f3a3be551d6fc982bc2963434a8f28a
Sean Ahn <sean.ahn@c3iot.com>
2021-09-07 11:49:55 -0700

    c3 - gen-schema.sh

---
+++ docker/c3-shell/gen-schema.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export TABLE_NAME=${TABLE_NAME-sean_test.raw}
+export BASE_LOCATION=${BASE_LOCATION-abfs://sean-test@stageazpaasstore01.dfs.core.windows.net}
+
+cat << EOF
+create external table $TABLE_NAME(
+  InstanceId string,
+  InstanceType string,
+  CpuCount string,
+  GpuCount string,
+  ts string,
+  Uptime string,
+  Metric string,
+  Resource string,
+  C3Id string,
+  Value string,
+  Provider string,
+  Env string,
+  Customer string,
+  Region string
+) partitioned by (cluster string, dt string)
+row format delimited fields terminated by ','
+stored as textfile;
+EOF
+
+function partition() {
+  if [ -n "$2" ]; then # folder depth of 2
+    cat << EOF
+ALTER TABLE $TABLE_NAME ADD PARTITION (cluster='$1', dt='$2')
+LOCATION '$BASE_LOCATION/$1/$2';
+EOF
+  fi
+}
+export -f partition
+
+tr '/' ' ' <&0 | xargs -l bash -c 'partition $0 $1'
--- docker/c3-shell/release.sh
+++ docker/c3-shell/release.sh
@@ -1,4 +1,4 @@
-export VER=1.5
+export VER=1.13
 
 docker build -t c3ai/c3-impala-shell:$VER .
 docker tag c3ai/c3-impala-shell:$VER c3ai/c3-impala-shell:latest
