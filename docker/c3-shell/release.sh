dbc5e52b1f87e092dd8a8bfcfca56f5f834331cf
Sean Ahn <c3-sahn@c3.ai>
2021-10-28 08:32:47 -0700

    c3 - fixed cronjob to do 5 day refresh

--- docker/c3-shell/release.sh
+++ docker/c3-shell/release.sh
@@ -1,4 +1,4 @@
-export VER=1.57
+export VER=1.61
 
 docker build -t c3ai/c3-impala-shell:$VER .
 docker tag c3ai/c3-impala-shell:$VER c3ai/c3-impala-shell:latest
--- docker/c3-shell/spark/partition.scala
+++ docker/c3-shell/spark/partition.scala
@@ -1,4 +1,6 @@
 import java.net.URI
+import java.time._
+import java.time.format._
 import org.apache.hadoop.fs._
 import org.apache.hadoop.conf.Configuration
 import sys.process._
@@ -6,6 +8,8 @@ import sys.process._
 def path = "wasb://c3telemetry@stageazpaasstore01.blob.core.windows.net/"
 // def path = "wasb://sean-test@stageazpaasstore01.blob.core.windows.net/"
 def fs = FileSystem.get(URI.create(path), new Configuration())
+
+var exitAfter = false
 
 def tenants(include: Set[String] = Set.empty) = {
   val bl = Set("c3telemetry", "sean-test", "")
@@ -22,22 +26,33 @@ def tenants(include: Set[String] = Set.empty) = {
   }.sorted
 }
 
-def partitions(prefix: String, key: String) = {
+def partitions(prefix: String, key: String)(implicit dates: (String, String)) = {
   val pattern = ".*/" + key + "=(\\d{" + key.length + "})"
-  val Pattern = pattern.r
+  val LeafValuePattern = pattern.r
+  val DatePattern = ".*?/(.*)".r
 
   fs.listStatus(new Path(s"$path$prefix")).map(
     _.getPath.toString
   ).map {
-    case Pattern(value) => s"$prefix/$key=$value"
+    case LeafValuePattern(value) => s"$prefix/$key=$value"
     case _ => null
   }.filter{
-    case partition if partition != null => true
+    /*
+     * when the range is (2021-12-28 - 2022-01-03),
+     * 2020 is not between (2012 - 2022)
+     * 2020-01 to 2021-11 is not between (2021-12 - 2022-01)
+     * 2021-12-01 to 2021-12-27 is not between (2021-12-28 to 2022-01-03)
+     * and so forth
+     */
+    case DatePattern(date: String) if 
+      date >=  dates._1.substring(0, date.length) && 
+      date <= dates._2.substring(0, date.length) => true
     case _ => false
   }
 }
 
-def copy(include: Set[String] = Set.empty, to: String = "sean-metry") = {
+def copy(include: Set[String] = Set.empty, to: String = "sean-metry")
+        (implicit dates: (String, String) = ("yyyy=2020/mm=01/dd=01", "yyyy=2040/mm=12/dd=31")) = {
   val dest = s"wasb://$to@stageazpaasstore01.blob.core.windows.net/"
   def fs2 = FileSystem.get(URI.create(dest), new Configuration())
 
@@ -49,12 +64,16 @@ def copy(include: Set[String] = Set.empty, to: String = "sean-metry") = {
   }.collect
 }
 
-def process(dryRun: Boolean = true, include: Set[String] = Set.empty) = {
+def process(dryRun: Boolean = true, include: Set[String] = Set.empty)
+           (implicit dates: (String, String) = ("yyyy=2020/mm=01/dd=01", "yyyy=2040/mm=12/dd=31")) = {
+  println(s"Processing between ${dates._1} and ${dates._2}...")
+
   val DROP_PARTITION = "ALTER TABLE c3telemetry.raw DROP IF EXISTS PARTITION"
   val ADD_PARTITION = "ALTER TABLE c3telemetry.raw ADD PARTITION"
   val Pattern = "(.*)/yyyy=(.*)/mm=(.*)/dd=(.*)".r
-  val output = "/tmp/out/" + scala.util.Random.alphanumeric.take(8).mkString("")
-  val logs = "/tmp/log/" + scala.util.Random.alphanumeric.take(8).mkString("")
+  val runId = scala.util.Random.alphanumeric.take(8).mkString("")
+  val output = s"/tmp/spark/$runId/out"
+  val logs = s"/tmp/spark/$runId/log"
   s"mkdir -p $logs".!
 
   // make tenant + year as a spark partition
@@ -72,12 +91,42 @@ def process(dryRun: Boolean = true, include: Set[String] = Set.empty) = {
 
   // parallelize importing of the files into impala
   rdd.mapPartitionsWithIndex { (index, itr) =>
-    val command = s"impala -f $output/part-" + "%05d".format(index) + s" > $logs 2>&"
+    val file = s"$output/part-" + "%05d".format(index)
+    val countCommand = s"wc -l $file"
+    val importCommand = s"impala -f $file > $logs 2>&"
+    val lines = countCommand.!
     if(dryRun) {
-      s"echo [${Thread.currentThread.getId}] $command".!
+      s"echo [${Thread.currentThread.getId}] $importCommand".!
     } else {
-      command.!
+      importCommand.!
     } 
     itr // got to return an iterator of string per mapPartitionsWithIndex signature
   }.collect
+}
+
+// limit the date range
+def processForDays(days: Int = 5, dryRun: Boolean = true, include: Set[String] = Set.empty) = {
+  try {
+    val min = java.time.LocalDate.now.minusDays(days - 2)
+    val max = min.plusDays(days)
+
+    val datef = java.time.format.DateTimeFormatter.ofPattern("'yyyy'=yyyy'/mm'=MM'/dd'=dd");
+    implicit val dates = (min.format(datef), max.format(datef))
+    process(dryRun, include)
+  } catch {
+    case e: Exception => 
+      e.printStackTrace
+      if(exitAfter) System.exit(1)
+  } finally {
+    if(exitAfter) System.exit(0)
+  }
+}
+
+// run daily incremental partition refresh 
+sys.env.get("SHELL_EXEC_MODE") match {
+  case Some(v) if v == "DAILY" => 
+    println("Running daily incremental partition refresh...")
+    exitAfter = true
+    processForDays()
+  case _ => // nothing
 }
